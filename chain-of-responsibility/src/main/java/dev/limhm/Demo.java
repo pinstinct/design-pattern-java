bea919814830e9921e9780e36f2399bbfda935c4
pinstinct <contact@limhm.dev>
2025-06-09 11:29:46 +0900

    책임 연쇄 패턴

--- chain-of-responsibility/src/main/java/dev/limhm/Demo.java
+++ chain-of-responsibility/src/main/java/dev/limhm/Demo.java
@@ -1,10 +1,46 @@
 package dev.limhm;
 
+import dev.limhm.middleware.Middleware;
+import dev.limhm.middleware.RoleCheckMiddleware;
+import dev.limhm.middleware.ThrottlingMiddleware;
+import dev.limhm.middleware.UserExistsMiddleware;
+import dev.limhm.server.Server;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
+
+/**
+ * Demo class. Everything comes together here.
+ */
 public class Demo {
 
+  private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  private static Server server;
+
+  private static void init() {
+    server = new Server();
+    server.register("admin@example.com", "admin_pass");
+    server.register("user@example.com", "user_pass");
+
+    // All checks are linked. Client can build various chains using the same component.
+    Middleware middleware = Middleware.link(new ThrottlingMiddleware(2),
+        new UserExistsMiddleware(server), new RoleCheckMiddleware());
+
+    // Server gets a chain from client code.
+    server.setMiddleware(middleware);
+  }
+
   public static void main(String[] args) throws IOException {
-    System.out.println("hello");
+    init();
+
+    boolean success;
+    do {
+      System.out.println("Enter email: ");
+      String email = reader.readLine();
+      System.out.println("Input password: ");
+      String password = reader.readLine();
+      success = server.login(email, password);
+    } while (!success);
   }
 }
---
+++ chain-of-responsibility/src/main/java/dev/limhm/middleware/Middleware.java
@@ -0,0 +1,36 @@
+package dev.limhm.middleware;
+
+/**
+ * Base  middleware class.
+ */
+public abstract class Middleware {
+
+  private Middleware next;
+
+  /**
+   * Builds chains of middleware objects.
+   */
+  public static Middleware link(Middleware first, Middleware... chain) {
+    Middleware head = first;
+    for (Middleware nextInChain : chain) {
+      head.next = nextInChain;
+      head = nextInChain;
+    }
+    return first;
+  }
+
+  /**
+   * Subclass will implement this method with concrete checks.
+   */
+  public abstract boolean check(String email, String password);
+
+  /**
+   * Runs check on the next object in chain or end traversing if we're in last object in chain.
+   */
+  protected boolean checkNext(String email, String password) {
+    if (next == null) {
+      return true;
+    }
+    return next.check(email, password);
+  }
+}
